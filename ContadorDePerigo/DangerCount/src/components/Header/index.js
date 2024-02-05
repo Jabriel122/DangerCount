2e4168b2db8a5e65e0b52b3b7c48a78f5b2ac813
Gabriel <gbielmarch@gmail.com>
2024-02-05 11:03:37 -0300

    ABLUAH ABLUAH

--- ContadorDePerigo/DangerCount/src/components/Header/index.js
+++ ContadorDePerigo/DangerCount/src/components/Header/index.js
@@ -2,21 +2,15 @@ import { useState } from "react";
 import { ContainerCount, ContainerNivel, TextCount, TextNivel, bomTheme, muitoBomTheme, normalTheme, ruimTheme, terrivelTheme } from "./styled";
 
 
-export function Header({ nivelText, count,theme}) {
-    // const [theme,setTheme] = useState("")
-    //ContainerNivel - View
-        //TextNivel - Text
-    //ContainerCount - View
-        //TextCount - Text
+export function Header({
+     nivelText, 
+     count,
+    
+    }) {
 
-
-    //********** Função que provavelmente deu errado, mas estou salvando ela aqui *********************/
-    // const ThemeChange = () =>{
-    //     theme == count < 5 ? setTheme('terrivel') : count < 10 ? setTheme('ruim') : count < 15 ? setTheme('normal') : count < 20 ? setTheme('bom') : setTheme('muito bom')
-    // }
     return (
         <>
-            <ContainerNivel theme={theme == 'terrivel' ? terrivelTheme : theme == 'ruim' ? ruimTheme : theme == 'normal' ? normalTheme : theme == 'bom' ? bomTheme : muitoBomTheme}>
+            <ContainerNivel theme>
                 <TextNivel > {nivelText} </TextNivel>
             </ContainerNivel>
             <ContainerCount>
--- ContadorDePerigo/DangerCount/src/components/Header/styled.js
+++ ContadorDePerigo/DangerCount/src/components/Header/styled.js
@@ -5,7 +5,7 @@ import styled from "styled-components";
 export const ContainerNivel = styled.View`
 
     width: 80%;
-    /* background-color: #F5A19E; */
+    background-color: ${props => `${props.theme}` };
     align-items: center;
     border-radius: 12px 12px 0px 0px ;
 
@@ -16,21 +16,21 @@ export const ContainerNivel = styled.View`
     shadow-radius:15px;
     elevation: 5;
 `
-export const terrivelTheme = {
-    backgroundColor:'#F5A19E',
-}
-export const ruimTheme = {
-    backgroundColor:'#F5BCC7',
-}
-export const normalTheme = {
-    backgroundColor:'#F5EFA9',
-}
-export const bomTheme = {
-    backgroundColor:'#D6F4DB',
-}
-export const muitoBomTheme = {
-    backgroundColor:'#69F58D',
-}
+// export const terrivelTheme = {
+//     backgroundColor:'#F5A19E',
+// }
+// export const ruimTheme = {
+//     backgroundColor:'#F5BCC7',
+// }
+// export const normalTheme = {
+//     backgroundColor:'#F5EFA9',
+// }
+// export const bomTheme = {
+//     backgroundColor:'#D6F4DB',
+// }
+// export const muitoBomTheme = {
+//     backgroundColor:'#69F58D',
+// }
 
 export const TextNivel = styled.Text`
     font-size: 36px;
